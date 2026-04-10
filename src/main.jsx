7dcf40e186db8ef7e3a6d21f4595e7c9e35fb0d1
maniyarrumaan-debug <maniyarrumaan@gmail.com>
2026-04-10 08:58:32 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CharteredReelsApp from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CharteredReelsApp />
+);
